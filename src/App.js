869fa667e00816b4a551ead6f732c851ea7d0909
Sara Aerssens <s.c.aerssens@gmail.com>
2020-04-02 17:07:14 +0200

    refactored categories filter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Board from './Board';
-import Filter from './Filter';
 import moment from 'moment';
 
+const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 const date = moment().format("dddd MMMM D, YYYY");
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1 className="app-title">Daily News</h1>
-        <Filter />
-        <div className="date">{moment().format("dddd MMMM D, YYYY")}</div>
-        <hr className="app-line"/>
-      </header>
-      <Board />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      category: null
+    }
+
+    this.loadCategories = this.loadCategories.bind(this);
+  }
+
+  loadCategories() {
+    return categories.map((cat, i) => <button type="button" className="category-btn" key={i}>{cat}</button>);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Daily News</h1>
+          {/* <Filter /> */}
+          <div className="categories">{this.loadCategories()}</div>
+          <div className="date">{date}</div>
+          <hr className="app-line"/>
+        </header>
+        <Board category={this.state.category} />
+      </div>
+    );
+  }
 }
 
 export default App;
